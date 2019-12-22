c8df7290a8454ef7b120cd2993247de287dc169c
vrushya <vrushabhdeshmukh3@gmail.com>
2019-12-22 23:54:22 +0530

    create navigation bootstrap view

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
+gem 'bootstrap-sass', '~> 3.4.1'
+gem 'sassc-rails', '>= 2.1.0'
+gem 'jquery-rails'
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
 gem 'sass-rails', '>= 6'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,43 +1,53 @@
 class ArticlesController < ApplicationController
-       def new
-        @article = Article.new
-       end
-       
-       def show
-         @article = Article.find(params[:id])
-       end
-     
-       def create
-        @article = Article.new(article_params)
-        if @article.save
-           flash[:notice] = "Article was successfully created"
-           redirect_to article_path(@article)
-        else
-           render 'new'
-        end
-       end
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-       def edit
-         @article = Article.find(params[:id])
-       end
+def new
+  @article = Article.new
+end
 
-       def index
-         @articles = Article.all
-       end
+def create
+    @article = Article.new(article_params)
+     if @article.save
+       flash[:notice] = "Article was successfully created"
+       redirect_to article_path(@article)
+     else
+       render 'new'
+  end
+end
 
-      def update
-         @article = Article.find(params[:id])
-         if @article.update(article_params)
-          flash[:notice] = "Article was updated"
-          redirect_to article_path(@article)
-         else
-          flash[:notice] = "Article was not updated"
-          render 'edit'
-         end
-       end
+   def show
+   
+   end
 
- private
+def update
+    if @article.update(article_params)
+     flash[:notice] = "Article was updated"
+     redirect_to article_path(@article)
+  else
+     flash[:notice] = "Article was not updated"
+   render 'edit'
+  end
+end
+
+def edit
+end
+
+def index
+  @articles = Article.all
+end
+
+def destroy
+  @article.destroy
+  flash[:notice] = "Article was deleted"
+  redirect_to articles_path
+end
+
+private
   def article_params
    params.require(:article).permit(:title, :description)
+  end
+  
+def set_article
+   @article = Article.find(params[:id])
   end
 end
